f3486e970f3f2f14eef35edff2b405ea2505c87c
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-02-26 17:22:15 -0300

    class Header rinex

--- src/rinex3/header_attrs.py
+++ src/rinex3/header_attrs.py
@@ -1,13 +1,6 @@
 import RINExplorer as rx
 
-infile = 'G:\\Meu Drive\\Python\\data-analysis\\database\\GNSS\\'
 
-filename = 'AREG00PER_R_20190860000_01D_30S_MO.rnx'
-filename = 'GLPS00ECU_R_20220010000_01D_30S_MO.rnx'
-lines = open(infile + filename, "r").readlines()
-
-obs_types = []
-time_of_obs = {}
 
 def convert_to_datetime(dic):
     '''
@@ -18,30 +11,60 @@ def convert_to_datetime(dic):
         dic[key] = rx.get_datetime(dic[key])
     return dic
 
-for num, ln in enumerate(lines):
+class HeaderRINEX3:
     
-    infos = ln[:60]
-    infos_reader = ln[60:].strip()
     
-    if 'END OF HEADER' in ln:
-        break
-    else:        
-        if 'OBS TYPES' in ln:
-            obs_types.append(infos)
+    
+    def __init__(self, infile):
+        
+    
+        lines = open(infile, "r").readlines()
+        
+        obs_types = []
+        time_of_obs = {}
+        geral_infos = {}
+        
+        for num, ln in enumerate(lines):
             
-        elif 'TIME' in ln:
-            time_of_obs[infos_reader[8:-4].lower()] = infos.split()
+            infos = ln[:60]
+            infos_reader = ln[60:].strip()
             
+            if 'END OF HEADER' in ln:
+                break
+            else:        
+                if 'OBS TYPES' in ln:
+                    obs_types.append(infos)
+                    
+                elif 'TIME' in ln:
+                    time_of_obs[infos_reader[8:-4].lower()] = infos.split()
+                
+                elif 'APPROX POSITION XYZ' in ln:
+                    geral_infos['position'] = infos.split()
+                
+                elif 'VERSION' in ln:
+                    geral_infos['version'] = infos[:10].strip()
+                    
+                elif 'INTERVAL' in ln:
+                    geral_infos['interval'] = infos.strip()
+                    
+                elif 'MARKER NAME' in ln:
+                    geral_infos['code'] = infos.strip()
+                    
+        
+        
+        geral_infos.update(convert_to_datetime(time_of_obs))
+        
+        self.obs_types = rx.join_obs_types(obs_types)
+        self.header = geral_infos
+        
 
 
-
-obs_types_joined = rx.join_obs_types(obs_types)
-
-
-
-
-# infos[:47].strip().split()
-# obs_types_joined
-
-time_of_obs = convert_time_of_obs_to_datetime(time_of_obs)
-
+def main():
+    infile = 'G:\\Meu Drive\\Python\\data-analysis\\database\\GNSS\\'
+    
+    filename = 'AREG00PER_R_20190860000_01D_30S_MO.rnx'
+    filename = 'GLPS00ECU_R_20220010000_01D_30S_MO.rnx'
+    
+    
+    path = infile + filename
+    HeaderRINEX3(path).obs_types
