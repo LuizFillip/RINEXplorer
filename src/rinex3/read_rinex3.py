70b90d160924ea98be3ad5631462a718705806b2
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-02-28 12:08:00 -0300

    concat observables and lli data

--- src/rinex3/read_rinex3.py
+++ src/rinex3/read_rinex3.py
@@ -9,6 +9,7 @@ class RINEX3(object):
         
         
         ep = rx.data_epochs(path_file)
+        
         self.types = ep.obs_types
         self.header = ep.header
         self.prn, self.index, self._obs, self._lli, self._ssi = ep.data_section
@@ -16,58 +17,84 @@ class RINEX3(object):
     def prns(self, const = 'G'):
         return np.unique(self.prn[const])
     
-    @staticmethod
-    def dataset(values, obs_names, index, prn):
+
+    def obs(self, values = 'obs', const = 'G', drop_ssi = True):
+        
+        if values == 'obs':
+            data = self._obs
+            
+        elif values == 'lli':
+            data = self._lli
+            
+        else:
+            data = self._ssi
+            
         df = pd.DataFrame(
-            values, 
-            columns = obs_names, 
-            index = index
-            )
-        df['prn'] = prn
+             data[const], 
+             columns = self.types[const], 
+             index = self.index[const]
+             )
+        
+        df['prn'] = self.prn[const]
+        
+        if drop_ssi:
+            columns = [col for col in df.columns if 'S' in col]
+            df = df.drop(columns = columns)
+        
+        
         return df
 
-    def obs(self, const = 'G'):
-        return self.dataset(
-            self._obs[const], 
-            self.types[const], 
-            self.index[const], 
-            self.prn[const]
-            )
-
-    def lli(self, const = 'G'):
-        return self.dataset(
-            self._lli[const], 
-            self.types[const], 
-            self.index[const], 
-            self.prn[const]
-            )
-
-    def ssi(self, const = 'G'):
-        return self.dataset(
-            self._ssi[const], 
-            self.types[const], 
-            self.index[const], 
-            self.prn[const])
     
-    @staticmethod
-    def sel(self, df, prn):            
-        return df.loc[df["prn"] == prn]
+    def sel(self, prn, num = 0, drop_ssi = True):           
+        
+        
+        df = self.obs(
+            values = 'obs', 
+            const = prn[0], 
+            drop_ssi = drop_ssi
+            )
+        
+        df = df.loc[df["prn"] == prn]
+        
+        columns_with_all_nan = df.columns[
+            df.isna().sum() == len(df)]
+                        
+        df = df.drop(columns = columns_with_all_nan)
+        
+        cols = rx.filter_columns(df)[num]
+        
+        lli = self.obs(
+            values = 'lli', 
+            const = prn[0], 
+            drop_ssi = drop_ssi
+            )
+        
+        lli.columns = [f'{c}lli' for c in lli.columns]
+        lli_cols = [f'{c}lli' for c in cols if 'L' in c]
+        
+        lli = lli.loc[lli["prnlli"] == prn, lli_cols]
+        
+        
+        return pd.concat([df.loc[:, cols], lli], axis = 1)
+        
 
  
 
 
 
+def main():
 
-
-infile = 'G:\\Meu Drive\\Python\\data-analysis\\database\\GNSS\\'
-
-filename = 'AREG00PER_R_20190860000_01D_30S_MO.rnx'
-filename = 'GLPS00ECU_R_20220010000_01D_30S_MO.rnx'
-
-path_file = infile + filename 
-
-df = RINEX3(path_file)
-
-ds = df.obs(const = 'R')
-
-df.sel()
+    infile = 'G:\\Meu Drive\\Python\\data-analysis\\database\\GNSS\\'
+    
+    filename = 'AREG00PER_R_20190860000_01D_30S_MO.rnx'
+    filename = 'GLPS00ECU_R_20220010000_01D_30S_MO.rnx'
+    
+    path_file = infile + filename 
+    
+    rinex = RINEX3(path_file)
+    
+    prn = 'G01'
+    
+    df = rinex.sel(prn)
+    
+    df
