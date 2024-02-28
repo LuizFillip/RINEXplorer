6237c6fc591fe2720dd229f641703be290ee4ddd
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-02-28 15:41:29 -0300

    deleting conbination in dict

--- src/rinex3/codes_pairs.py
+++ src/rinex3/codes_pairs.py
@@ -1,3 +1,10 @@
+channels = {'C': [], 'W': [], 'X': [], 
+               'L': [], 'Z': [], 'I': [], 
+               'E': [], 'P': [], 'Q': [], 
+               'A': [], 'B': [], 'M': [], 
+               'N': [], 'Y': []}
+   
+
 def observation_codes_pairs(df):
     codes = {'L': [], 'C': []}
     
@@ -13,39 +20,53 @@ def observation_codes_pairs(df):
                 
 
 
-def combination_channel_pairs(codes, complete_pairs = True):
-    
-    channels = {'C': [], 'W': [], 'X': [], 
-                'L': [], 'Z': [], 'I': [], 
-                'E': [], 'P': [], 'Q': [], 
-                'A': [], 'B': [], 'M': [], 
-                'N': [], 'Y': []}
-    
-    
-    for l, c in zip(codes['L'], codes['C']):
-        if l[2] != c[2]:
-            pass 
-        else:
-            for channel in channels.keys():
-                if channel == c[2]:
-                    channels[channel].append(c)
-                if channel == l[2]:
-                    channels[channel].append(l)
-                    
-                    
-    cols = {key: value for key, value in channels.items() if value}
 
-    if complete_pairs:
-        cols = {key: value for key, value in 
-                channels.items() if len(value) == 4}
-        
-    return cols
-
-
-def filter_columns(df):
+def filter_columns(df, sel = 1):
     
     codes = observation_codes_pairs(df)
     
-    pairs = combination_channel_pairs(codes)
-        
-    return list(pairs.values())
+    comb = combine_pairs(codes)
+    
+    if sel == 1:
+        sel_cols = list(comb[1][0] + comb[2][0])
+    else:
+        sel_cols = list(comb[1][0] + comb[5][0])
+    
+    return sel_cols
+
+def combine_pairs(codes):
+    
+    out = {1: [], 2: [], 5: []}
+    
+    for l in codes['L']:
+        for c in codes['C']:
+            if l[1] != c[1]:
+                continue
+            if l[2] == c[2]:
+                for key in out.keys():
+                    if key == int(l[1]):
+                        out[key].append((l, c))
+                        
+    return out 
+
+def main():
+    import RINExplorer as rx 
+    infile = 'G:\\Meu Drive\\Python\\data-analysis\\database\\GNSS\\'
+    
+    filename = 'AREG00PER_R_20190860000_01D_30S_MO.rnx'
+    # filename = 'GLPS00ECU_R_20220010000_01D_30S_MO.rnx'
+    
+    path_file = infile + filename 
+    
+    rinex = rx.RINEX3(path_file)
+    
+    prn = 'G01'
+    
+    df = rinex.sel(prn)
+    
+    
+    
+    codes = observation_codes_pairs(df)
+    
+
+
