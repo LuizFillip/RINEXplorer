1b5f1d72337468facc7cf1b1b516546322091af2
Luiz Fillip Rodrigues Vital <72110299+LuizFillip@users.noreply.github.com>
2024-03-05 10:57:13 -0300

    change names

--- src/rinex3/__init__.py
+++ src/rinex3/__init__.py
@@ -2,4 +2,4 @@ from .header_tools import join_obs_types
 from .header_attrs import HeaderRINEX3
 from .observables import data_epochs
 from .codes_pairs import combine_pairs
-from .read_rinex3 import RINEX3
+from .read_rinex3 import rinex3
